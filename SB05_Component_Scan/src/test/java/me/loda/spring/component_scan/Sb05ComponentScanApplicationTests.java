8e9b8a54914b70ff5bc630d0f2d2945b4f7fed7d
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-02 14:55:26 +0700

    [SB5] Component Scan la gi?

---
+++ SB05_Component_Scan/src/test/java/me/loda/spring/component_scan/Sb05ComponentScanApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loda.spring.component_scan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb05ComponentScanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
