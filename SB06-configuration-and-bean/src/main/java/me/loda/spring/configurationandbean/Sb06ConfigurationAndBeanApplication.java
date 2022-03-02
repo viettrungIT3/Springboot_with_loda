bbdde504c8b4e610955a129b0ca3d1f29804a35f
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-02 16:19:46 +0700

    [SB6] @Configuration và @Bean

---
+++ SB06-configuration-and-bean/src/main/java/me/loda/spring/configurationandbean/Sb06ConfigurationAndBeanApplication.java
@@ -0,0 +1,13 @@
+package me.loda.spring.configurationandbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb06ConfigurationAndBeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb06ConfigurationAndBeanApplication.class, args);
+    }
+
+}
---
+++ SB06-configuration-and-bean/src/test/java/me/loda/spring/configurationandbean/Sb06ConfigurationAndBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loda.spring.configurationandbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb06ConfigurationAndBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
