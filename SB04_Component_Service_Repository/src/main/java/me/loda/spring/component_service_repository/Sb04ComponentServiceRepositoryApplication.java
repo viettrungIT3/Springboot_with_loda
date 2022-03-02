544d5685382a889f9147572da3b9ab3045e2bf5c
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-02 14:12:53 +0700

    [SB4] @Component vs @Service vs @Repository

---
+++ SB04_Component_Service_Repository/src/main/java/me/loda/spring/component_service_repository/Sb04ComponentServiceRepositoryApplication.java
@@ -0,0 +1,13 @@
+package me.loda.spring.component_service_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb04ComponentServiceRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb04ComponentServiceRepositoryApplication.class, args);
+    }
+
+}
---
+++ SB04_Component_Service_Repository/src/test/java/me/loda/spring/component_service_repository/Sb04ComponentServiceRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loda.spring.component_service_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb04ComponentServiceRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
