a7cd6e9ccfb371de140fba6e74bba3277d399867
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-04 09:22:51 +0700

    [SB7] Spring Boot Application Config và @Value

---
+++ SB07_SpringBootApplication_Config/src/main/java/me/loda/spring/applicationsproperties/App.java
@@ -0,0 +1,16 @@
+package me.loda.spring.applicationsproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(App.class, args);
+
+        DatabaseConnector databaseConnector = context.getBean(DatabaseConnector.class);
+        databaseConnector.connect();
+    }
+}
---
+++ SB07_SpringBootApplication_Config/src/main/java/me/loda/spring/applicationsproperties/MySqlConnector.java
@@ -0,0 +1,8 @@
+package me.loda.spring.applicationsproperties;
+
+public class MySqlConnector extends DatabaseConnector {
+    @Override
+    public void connect() {
+        System.out.println("Đã kết nối tới Mysql: " + getUrl());
+    }
+}
