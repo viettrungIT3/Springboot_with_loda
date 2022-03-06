b459ee8bd2c5476e474272b78ec012012482e37d
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-06 20:12:19 +0700

    [SB12] Spring JPA Method + @Query

---
+++ SB12_SpringJPAMethod_Query/src/main/java/me/loda/spring/demo/App.java
@@ -0,0 +1,36 @@
+package me.loda.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(App.class, args);
+
+        UserRepository userRepository = context.getBean(UserRepository.class);
+
+        System.out.println("Tìm user với Agi trong khoảng (25 - 30)");
+        System.out.println("findAllByAgiBetween: ");
+        userRepository.findAllByAgiBetween(25, 30).forEach(System.out::println);
+
+        System.out.println("===========================================");
+        System.out.println("Tìm user với Agi trong lớn hơn 97");
+        System.out.println("findAllByAgiGreaterThan: ");
+        userRepository.findAllByAgiGreaterThan(97).forEach(System.out::println);
+
+        // Tìm kiếm tất cả theo Atk = 74
+        System.out.println("===========================================");
+        System.out.println("Tìm user với Atk = 74");
+        System.out.println("findAllByAtk: ");
+        userRepository.findAllByAtk(74).forEach(System.out::println);
+
+        System.out.println("===========================================");
+        System.out.println("Tìm User với def = 49");
+        System.out.println("SELECT u FROM User u WHERE u.def = :def");
+        User user = userRepository.findUserByDefQuery(49);
+        System.out.println(user);
+    }
+}
---
+++ SB12_SpringJPAMethod_Query/src/main/java/me/loda/spring/demo/UserRepository.java
@@ -0,0 +1,20 @@
+package me.loda.spring.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAllByAtk(int atk);
+
+    List<User> findAllByAgiBetween(int start, int end);
+
+    @Query("SELECT u FROM User u WHERE u.def = :def")
+    User findUserByDefQuery(@Param("def") Integer def);
+
+    List<User> findAllByAgiGreaterThan(int agiThreshold);
+}
