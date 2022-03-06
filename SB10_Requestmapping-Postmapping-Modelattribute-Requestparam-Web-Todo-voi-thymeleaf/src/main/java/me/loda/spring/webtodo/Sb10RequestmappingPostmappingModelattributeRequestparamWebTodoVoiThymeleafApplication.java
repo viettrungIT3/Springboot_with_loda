3be5510a89e4449b72a3f4e2697b8c6445e36840
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-06 08:07:54 +0700

    [SB10] @RequestMapping + @PostMapping + @ModelAttribute + @RequestParam + Web To-Do với Thymeleaf

---
+++ SB10_Requestmapping-Postmapping-Modelattribute-Requestparam-Web-Todo-voi-thymeleaf/src/main/java/me/loda/spring/webtodo/Sb10RequestmappingPostmappingModelattributeRequestparamWebTodoVoiThymeleafApplication.java
@@ -0,0 +1,13 @@
+package me.loda.spring.webtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb10RequestmappingPostmappingModelattributeRequestparamWebTodoVoiThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb10RequestmappingPostmappingModelattributeRequestparamWebTodoVoiThymeleafApplication.class, args);
+    }
+
+}
---
+++ SB10_Requestmapping-Postmapping-Modelattribute-Requestparam-Web-Todo-voi-thymeleaf/src/test/java/me/loda/spring/webtodo/Sb10RequestmappingPostmappingModelattributeRequestparamWebTodoVoiThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loda.spring.webtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb10RequestmappingPostmappingModelattributeRequestparamWebTodoVoiThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
