1fe8a3e42d6f661870bcbd1d821f9e912c67f957
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-06 10:50:19 +0700

    [SB11] Spring Boot JPA + MySQL

---
+++ SB11_SpringBoot_JPA-MySQL/src/test/java/me/loda/spring/jpamysql/Sb11SpringBootJpaMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loda.spring.jpamysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb11SpringBootJpaMySqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
