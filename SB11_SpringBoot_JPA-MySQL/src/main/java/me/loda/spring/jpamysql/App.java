c62123780a21628999573a7d916f53884c8459c5
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-06 14:55:57 +0700

    Update....

--- SB11_SpringBoot_JPA-MySQL/src/main/java/me/loda/spring/jpamysql/App.java
+++ SB11_SpringBoot_JPA-MySQL/src/main/java/me/loda/spring/jpamysql/App.java
@@ -15,5 +15,18 @@ public class App {
 
         // Lấy ra toàn bộ user trong db
         userRepository.findAll().forEach(System.out::println);
+
+        // Lưu user xuống database
+        User user = userRepository.save(new User());
+        // Khi lưu xong, nó trả về User đã lưu kèm theo Id.
+        System.out.println("User vừa lưu có ID: " + user.getId());
+
+        Long UserID = user.getId();
+        // Cập nhật user
+        user.setAgi(100);
+        // Update user
+        userRepository.save(user);
+
+        userRepository.findAll().forEach(System.out::println);
     }
 }
