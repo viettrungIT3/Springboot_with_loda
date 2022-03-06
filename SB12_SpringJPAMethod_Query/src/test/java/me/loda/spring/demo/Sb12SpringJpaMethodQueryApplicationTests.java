0b37203d95e437b2865a62ada58f3912e629ed0f
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-06 15:45:20 +0700

    [SB12] Spring JPA Method + @Query

---
+++ SB12_SpringJPAMethod_Query/src/test/java/me/loda/spring/demo/Sb12SpringJpaMethodQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loda.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb12SpringJpaMethodQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
