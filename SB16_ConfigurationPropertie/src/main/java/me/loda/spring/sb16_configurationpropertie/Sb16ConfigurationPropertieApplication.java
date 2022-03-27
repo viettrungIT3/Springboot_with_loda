bd637fb6cdb6a1c4ac6412e267475d9aa91ceddf
viettrungIT3 <viettrungcntt03@gmail.com>
2022-03-27 17:52:31 +0700

    [SB16] Hướng dẫn sử dụng @ConfigurationPropertie

---
+++ SB16_ConfigurationPropertie/src/main/java/me/loda/spring/sb16_configurationpropertie/Sb16ConfigurationPropertieApplication.java
@@ -0,0 +1,13 @@
+package me.loda.spring.sb16_configurationpropertie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb16ConfigurationPropertieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb16ConfigurationPropertieApplication.class, args);
+    }
+
+}
---
+++ SB16_ConfigurationPropertie/src/test/java/me/loda/spring/sb16_configurationpropertie/Sb16ConfigurationPropertieApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loda.spring.sb16_configurationpropertie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb16ConfigurationPropertieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
